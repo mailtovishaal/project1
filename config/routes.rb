c611d00e78a6060dba2b3ff9a4bbfc8e1493bf71
vishaal <vishaaltaleda1992@gmail.com>
2016-07-20 05:14:43 +0000

    added bootstrap sass

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'frontend#index'
+
+  get 'frontend/contact'
+
+  get 'frontend/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
